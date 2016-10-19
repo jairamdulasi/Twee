aae02202dea17532a32a9348303736b16c3f6120
jairamdulasi <jairamdulasi@gmail.com>
2016-10-19 17:31:29 +0530

    Add files via upload
    Coursera - R Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  matInv<-NULL
+  set <-function(y){
+    x<<-y
+    matInv <-NULL
+  }
+  get <-function(y) x
+  setinverse<-function(inverse)matInv <<-inverse
+  getinverse<-function() matInv 
+  list(set=set,get=get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  
+  matInv<-x$getinverse()
+  if(!is.null(matInv)) {
+    message("getting cached data")
+    return(matInv)
+    
+  }
+  mat<-x$get()
+  matInv <-solve(mat,...)
+  x$setinverse(matInv)
+  matInv
+}
